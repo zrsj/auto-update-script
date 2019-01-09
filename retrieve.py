1c9d499c0e3f799f2ca1f72cd20aab14854bd998
zrsj <c3283006@uon.edu.au>
2019-01-09 15:08:10 +1100

    retrieve.py now able to connect to postgresql

--- retrieve.py
+++ retrieve.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
-str = "Hello, world!";
-print(str);
+import psycopg2
+import sys
+import pprint
+
+def main():
+	conn_str = "host='localhost' dbname='northwind' user='postgres' password='admin'"
+	print("Connecting to database...")
+	conn = psycopg2.connect(conn_str)
+	cursor = conn.cursor()
+	print("Connected!")
+	cursor.execute("SELECT * from employees")
+	rec = cursor.fetchall()
+	pprint.pprint(rec)
+if __name__ == "__main__":
+	main() 
