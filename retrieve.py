1a32017869419fdb781be732bc5f3de09d911d53
zrsj <c3283006@uon.edu.au>
2019-01-10 16:42:29 +1100

    Now utilising apscheduler with cron-style scheduling

--- retrieve.py
+++ retrieve.py
@@ -2,6 +2,7 @@
 import psycopg2
 import sys
 import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
 from threading import Timer
 
 def query_exec(conn_str, query_direc, data_direc):
@@ -17,10 +18,14 @@ def query_exec(conn_str, query_direc, data_direc):
 		cursor.copy_expert(outquer, ofile)
 	conn.close()
 
-def timertest():
+def script_run():
 	conn_str = "host='localhost' dbname='northwind' user='postgres' password='admin'"
 	idir1 = "/home/uniwork/workforce/app1/sql/query.sql"
+	idir2 = "/home/uniwork/workforce/app2/sql/query.sql"
+	idir3 = "/home/uniwork/workforce/app3/sql/query.sql"
 	odir1 = "/home/uniwork/workforce/app1/data/results.csv"
+	odir2 = "/home/uniwork/workforce/app2/data/results.csv"
+	odir3 = "/home/uniwork/workforce/app3/data/results.csv"
 	print("Beginning execution...")
 	try:
 		query_exec(conn_str, idir1, odir1)
@@ -28,12 +33,14 @@ def timertest():
 		print("ERROR: execution failure")
 	print("Execution succesfully completed. Data CSV files created.")
 
-def callback_timer():
-	timertest()
-	Timer(30.0, callback_timer).start()
-
 def main():
-	callback_timer()
+	script_run();
+	sched = BlockingScheduler()
+	sched.add_job(script_run, 'cron', day='*', hour=16, minute=41)
+	try:
+		sched.start()
+	except (KeyboardInterrupt, SystemExit):
+		pass
 
 if __name__ == "__main__":
 	main() 
