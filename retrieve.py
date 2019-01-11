2d7678bfc3cfb03c433f14e55fbfc64379881d70
zrsj <c3283006@uon.edu.au>
2019-01-11 14:39:11 +1100

    query_exec fixed as per mborawi suggestion, takes command line arg for root dir

--- retrieve.py
+++ retrieve.py
@@ -8,24 +8,21 @@ def query_exec(conn_str, query_direc, data_direc):
 	cursor = conn.cursor()
 	conn.set_session(autocommit=True)
 	str = ""
-	with open(query_direc, "r") as ifile:
-		for line in ifile:
-			if line == '\n':
-				outquer = "COPY ({0}) TO STDOUT WITH CSV HEADER".format(str)
-				with open(data_direc, "w") as ofile:
-					cursor.copy_expert(outquer, ofile)
-				str = ""
-			str += line
+	with cursor as cursor:
+		str = open(query_direc).read()
+		outquer = "COPY ({0}) TO STDOUT WITH CSV HEADER".format(str)
+		with open(data_direc, "w") as ofile:
+			cursor.copy_expert(outquer, ofile)
 	conn.close()
 
-def script_run():
+def script_run(rootdir = "./workforce"):
 	conn_str = "host='localhost' dbname='northwind' user='postgres' password='admin'"
-	idir1 = "/home/uniwork/workforce/app1/sql/query.sql"
-	idir2 = "/home/uniwork/workforce/app2/sql/query.sql"
-	idir3 = "/home/uniwork/workforce/app3/sql/query.sql"
-	odir1 = "/home/uniwork/workforce/app1/data/results.csv"
-	odir2 = "/home/uniwork/workforce/app2/data/results.csv"
-	odir3 = "/home/uniwork/workforce/app3/data/results.csv"
+	idir1 = rootdir + "/app1/sql/query.sql"
+	idir2 = rootdir + "/app2/sql/query.sql"
+	idir3 = rootdir + "/app3/sql/query.sql"
+	odir1 = rootdir + "/app1/data/results.csv"
+	odir2 = rootdir + "/app2/data/results.csv"
+	odir3 = rootdir + "/app3/data/results.csv"
 	try:
 		query_exec(conn_str, idir1, odir1)
 		query_exec(conn_str, idir2, odir2)
@@ -34,7 +31,10 @@ def script_run():
 		print("ERROR: execution failure")
 
 def main():
-	script_run();
+	try:
+		script_run(sys.argv[1]);
+	except:
+		script_run();
 	sched = BlockingScheduler()
 	sched.add_job(script_run, 'cron', year='*', month='*',  day='*', hour=7, minute=00)
 	try:
@@ -43,4 +43,4 @@ def main():
 		pass
 
 if __name__ == "__main__":
-	main() 
+	main()
